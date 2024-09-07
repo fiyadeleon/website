979aad2f67f60d223ac2fc04654518cd34ee6f3d
fiyadeleon <deleonfiya@gmail.com>
2024-09-08 00:39:19 +0800

    add lambda configs for users

--- aws/lambda/users/outputs.tf
+++ aws/lambda/users/outputs.tf
@@ -1,15 +1,3 @@
-output "get_inventory_invoke_arn" {
-  value = aws_lambda_function.get_inventory.invoke_arn
-}
-
-output "post_inventory_invoke_arn" {
-  value = aws_lambda_function.post_inventory.invoke_arn
-}
-
-output "put_inventory_invoke_arn" {
-  value = aws_lambda_function.put_inventory.invoke_arn
-}
-
-output "delete_inventory_invoke_arn" {
-  value = aws_lambda_function.delete_inventory.invoke_arn
+output "get_users_invoke_arn" {
+  value = aws_lambda_function.get_users.invoke_arn
 }
